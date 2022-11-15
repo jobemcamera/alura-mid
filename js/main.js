701e014c708bf1a3625371601933d0e22f7bfdeb
jobemcamera <jobecamera@gmail.com>
2022-11-14 21:40:55 -0300

    condição de erro na função tocaSom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    // verifica se elemento é nulo e do tipo audio
+    if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 }
 
 // lista criada com todos os elementos com a class 'tecla'
@@ -14,8 +21,21 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     // template string
     const idAudio = `#som_${instrumento}`;
 
-    // função anônima foi atribuída no evento onclick
+    // função anônima foi atribuída ao evento onclick
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    // função anônima foi atribuída ao evento onkeydown
+    tecla.onkeydown = function (evento) {
+        // condição das telcas (enter e espaço)
+        if (evento.conde === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    // função anônima foi atribuída ao evento onkeyup
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
